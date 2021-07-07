dd60a6b3e9df84b29d0c517096f3f89ffb76f95d
drcrally <jameswhitfield65@gmail.com>
2021-07-07 12:24:48 -0500

    hang

--- requiem/hang.py
+++ requiem/hang.py
@@ -1,87 +1,80 @@
-#importing the time module
-import time
-
-#welcoming the user
-name = raw_input("What is your name? ")
-
-print "Hello, " + name, "Time to play hangman!"
-
-print "
-"
-
-#wait for 1 second
-time.sleep(1)
-
-print "Start guessing..."
-time.sleep(0.5)
-
-#here we set the secret
-word = "secret"
-
-#creates an variable with an empty value
+import random
+# library that we use in order to choose
+# on random words from a list of words
+ 
+name = input("What is your name? ")
+# Here the user is asked to enter the name first
+ 
+print("Good Luck ! ", name)
+ 
+words = ['rainbow', 'computer', 'science', 'programming',
+         'python', 'mathematics', 'player', 'condition',
+         'reverse', 'water', 'board', 'geeks']
+ 
+# Function will choose one random
+# word from this list of words
+word = random.choice(words)
+ 
+ 
+print("Guess the characters")
+ 
 guesses = ''
-
-#determine the number of turns
-turns = 10
-
-# Create a while loop
-
-#check if the turns are more than zero
-while turns > 0:         
-
-    # make a counter that starts with zero
-    failed = 0             
-
-    # for every character in secret_word    
-    for char in word:      
-
-    # see if the character is in the players guess
-        if char in guesses:    
-    
-        # print then out the character
-            print char,    
-
+ 
+# any number of turns can be used here
+turns = 12
+ 
+ 
+while turns > 0:
+     
+    # counts the number of times a user fails
+    failed = 0
+     
+    # all characters from the input
+    # word taking one at a time.
+    for char in word:
+         
+        # comparing that character with
+        # the character in guesses
+        if char in guesses:
+            print(char)
+             
         else:
-    
-        # if not found, print a dash
-            print "_",     
-       
-        # and increase the failed counter with one
-            failed += 1    
-
-    # if failed is equal to zero
-
-    # print You Won
-    if failed == 0:        
-        print "
-You won"  
-
-    # exit the script
-        break              
-
-    print
-
-    # ask the user go guess a character
-    guess = raw_input("guess a character:") 
-
-    # set the players guess to guesses
-    guesses += guess                    
-
-    # if the guess is not found in the secret word
-    if guess not in word:  
+            print("_")
+             
+            # for every failure 1 will be
+            # incremented in failure
+            failed += 1
+             
  
-     # turns counter decreases with 1 (now 9)
-        turns -= 1        
- 
-    # print wrong
-        print "Wrong
-"    
- 
-    # how many turns are left
-        print "You have", + turns, 'more guesses' 
- 
-    # if the turns are equal to zero
-        if turns == 0:           
-    
-        # print "You Lose"
-            print "You Lose
+    if failed == 0:
+        # user will win the game if failure is 0
+        # and 'You Win' will be given as output
+        print("You Win")
+         
+        # this print the correct word
+        print("The word is: ", word)
+        break
+     
+    # if user has input the wrong alphabet then
+    # it will ask user to enter another alphabet
+    guess = input("guess a character:")
+     
+    # every input character will be stored in guesses
+    guesses += guess
+     
+    # check input with the character in word
+    if guess not in word:
+         
+        turns -= 1
+         
+        # if the character doesn’t match the word
+        # then “Wrong” will be given as output
+        print("Wrong")
+         
+        # this will print the number of
+        # turns left for the user
+        print("You have", + turns, 'more guesses')
+         
+         
+        if turns == 0:
+            print("You Loose")
